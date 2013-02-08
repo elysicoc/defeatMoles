832e7a253acc778a0ec960c4f42415a3e1767bf9
kiarashplusplus <kiarashplusplus@gmail>
2013-02-08 04:38:25 -0500

    extract skin LAB

--- demo.py
+++ demo.py
@@ -39,10 +39,18 @@ def templateMatching(img, coo, offx, offy):
 	
 	temp=img[coo[0]-offx:coo[0]+offx, coo[1]-offy:coo[1]+offy]
 	
-	#imshow(temp,cmap = plt.get_cmap('gray'))
-	#show()
+	imshow(temp,cmap = plt.get_cmap('gray'))
+	show()
+	ans=img.copy()
+	shapex,shapey=temp.shape
 	
+	'''for x in range(img.shape[0]-shapex):
+		for y in range(img.shape[1]-shapey):
+			ans[x,y]=sum(abs(img[x:x+shapex,y:y+shapey]-temp))
+	
+	'''
 	floatimg=img*1.0
+	print floatimg.shape, temp.shape
 	
 	ans=ndimage.convolve(floatimg, temp)
 	m=numpy.mean(ans)
@@ -55,14 +63,41 @@ def rgb2gray(rgb):
     return 0.299 * r + 0.587 * g + 0.114 * b
 	
 	
-img= imread("C:/Users/Kiarash/Dropbox/Git/defeatMoles/smallArm2.jpg")
-gray=rgb2gray(img).astype(uint8)
+from colormath.color_objects import *
+def skinDetection(img, treshold=80, color=[255,20,147]):
+
+	print img.shape
+	res=img.copy()
+	for x in range(img.shape[0]):
+		for y in range(img.shape[1]):
+			RGBimg=RGBColor(img[x,y,0],img[x,y,1],img[x,y,2])
+			LABimg=RGBimg.convert_to('lab', debug=False)
+			if (LABimg.lab_l > treshold):
+				res[x,y,:]=color
+			else: 
+				res[x,y,:]=img[x,y,:]
+
+	return res
+ 
+#img= imread("C:/Users/Kiarash/Dropbox/Git/defeatMoles/img012.jpg")
+
+#skin=skinDetection(img, 55)
+import cv
+import cv2
+im = cv.LoadImageM("C:/Users/Kiarash/Dropbox/Git/defeatMoles/img012.jpg", 1)
+dst=cv2.SimpleBlobDetector(im)
+cv.SaveImage("C:/Users/Kiarash/Dropbox/Git/defeatMoles/foo-laplace.png", dst)
+
+
+'''gray=rgb2gray(img).astype(uint8)
 norm=normalize_histogram(gray)
 
-coo=[579,769]
+coo=[1679,1350] #arm
+#coo=[580,775] #smallarm2
 
-match= templateMatching(norm, coo, 100,100) 
-imshow(match,cmap = plt.get_cmap('gray'))
+match= templateMatching(norm, coo, 100,100)
+ 
+imshow(match,cmap = plt.get_cmap('gray'))'''
 show()
 
 	
