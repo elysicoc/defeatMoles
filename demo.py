919bd3135f049a78313e5f5c397f158dced6345e
kiarashplusplus <kiarashplusplus@gmail>
2013-02-08 07:44:42 -0500

    matching

--- demo.py
+++ demo.py
@@ -81,31 +81,84 @@ def skinDetection(img, treshold=80, color=[255,20,147]):
 	return res
 	
 	
+
 import cv2
-def moleDetection(img, minArea=80):
+
+def moleDetection(img, minArea=30,maxArea=5000):
 	params=cv2.SimpleBlobDetector_Params()
-	params.filterByColor=False
+	params.filterByColor=True
 	params.filterByConvexity=False
 	params.filterByInertia=True
 	params.minArea=minArea
+	params.maxArea=maxArea	
 	detector=cv2.SimpleBlobDetector(params)
 	pts=detector.detect(img)
 
 	return pts
 	
 	
-img= imread("C:/Users/Kiarash/Dropbox/Git/defeatMoles/img012.jpg")
+b1= imread("C:/Users/Kiarash/Dropbox/Git/defeatMoles/back1.jpg")
+b0= imread("C:/Users/Kiarash/Dropbox/Git/defeatMoles/back0.jpg")
 
 #skin=skinDetection(img, 55)
 
-moles=moleDetection(img)
+moles0=moleDetection(b0)
+moles1=moleDetection(b1)
 
-while(len(moles)>0):
+extractor=cv2.DescriptorExtractor_create("SIFT")
+k1,d0=extractor.compute(b0, moles0)
+k2,d1=extractor.compute(b1, moles1)
+
+matcher=cv2.DescriptorMatcher_create("FlannBased")
+matches=matcher.match(d0,d1)
+
+
+# visualize the matches
+print '#matches:', len(matches)
+dist = [m.distance for m in matches]
+
+print 'distance: min: %.3f' % min(dist)
+print 'distance: mean: %.3f' % (sum(dist) / len(dist))
+print 'distance: max: %.3f' % max(dist)
+
+
+# threshold: half the mean
+thres_dist = 100
+
+# keep only the reasonable matches
+sel_matches = [m for m in matches if m.distance < thres_dist]
+
+print '#selected matches:', len(sel_matches)
+
+
+# #####################################
+# visualization
+h1, w1 = b0.shape[:2]
+h2, w2 = b1.shape[:2]
+view = zeros((max(h1, h2), w1 + w2, 3), uint8)
+view[:h1, :w1, 0] = b0[:,:,0]
+view[:h2, w1:, 0] = b1[:,:,0]
+view[:, :, 1] = view[:, :, 0]
+view[:, :, 2] = view[:, :, 0]
+
+
+for m in sel_matches:
+    # draw the keypoints
+    # print m.queryIdx, m.trainIdx, m.distance
+    color = tuple([random.randint(0, 255) for _ in xrange(3)]); print  k1[m.queryIdx].pt
+    cv2.line(view, (int(k1[m.queryIdx].pt[0]),int(k1[m.queryIdx].pt[1])), (int(k2[m.trainIdx].pt[0] + w1), int(k2[m.trainIdx].pt[1])), color)
+
+	
+	
+cv2.imshow("view", view)
+cv2.waitKey()
+
+
+'''while(len(moles)>0):
 	p=moles.pop().pt
 	gca().add_patch(Circle(p, 50, facecolor='none', edgecolor='r'))	
+'''
 
-
-imshow(img)
 
 
 '''gray=rgb2gray(img).astype(uint8)
